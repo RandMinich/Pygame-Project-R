411c662a195bce9da62eb4810225f49f6d4ee4ce
Rand Minich <75568329+RandMinich@users.noreply.github.com>
2021-12-21 13:54:19 +0300

    Level cycles

--- Levels.py
+++ Levels.py
@@ -6,6 +6,7 @@ class Level:
         self.bgi = Load_image.load_image(background_image)
         self.gravity = gravity
         self.objects = []
+        self.running
 
     def append_object(self, object):
         self.objects.append(object)
@@ -14,4 +15,5 @@ class Level:
         First_Screen.draw(self.bgi)
 
     def run_level(self, screen, is_running_flag):
-        pass
+        while sel.running:
+           ...
