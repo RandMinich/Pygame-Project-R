3ab8c97aa035378ff1468398b98a1553e0a88b79
Rand Minich <75568329+RandMinich@users.noreply.github.com>
2021-12-21 09:28:34 +0300

    Comments for load image

--- Load_image.py
+++ Load_image.py
@@ -3,10 +3,10 @@ import sys
 import pygame
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+def load_image(name, colorkey=None): # функция для правильной загрузки изображений
+    fullname = os.path.join('data', name) # создаём путь к файлу с изображением
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
+        print(f"Файл с изображением '{fullname}' не найден") # проверка на идиота
         sys.exit()
-    image = pygame.image.load(fullname)
-    return image
+    image = pygame.image.load(fullname) # загрузка
+    return image # отдаем
